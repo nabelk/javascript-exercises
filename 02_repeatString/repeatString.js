171c2778f8731254dc286d4cbe2429a46aae7b25
Nabil <holanabilkhalid@gmail.com>
2022-08-29 02:12:44 +0800

    Added statement and callback of the function to pass the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    while(num > 0){
+        result += string;
+        num--;
+    }
+   
+return result;
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10); 
+repeatString("hey", 1);
+
 
 // Do not edit below this line
 module.exports = repeatString;
