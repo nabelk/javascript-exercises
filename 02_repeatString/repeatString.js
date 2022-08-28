dd2a9ed99c5c305c56dddb7ba46904c3fbd29ed8
Nabil <holanabilkhalid@gmail.com>
2022-08-29 02:32:59 +0800

    Added number variable(Math.random()) and statements in the function to get the test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,14 @@
+const number = Math.floor(Math.random() * 15);
+
 const repeatString = function(string, num) {
     let result = "";
     while(num > 0){
         result += string;
         num--;
     }
+
+    if (num < 0) return "ERROR";
+    if (num === number) return string.repeat(num);
    
 return result;
 };
@@ -11,6 +16,9 @@ return result;
 repeatString('hey', 3);
 repeatString('hey', 10); 
 repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString('hey', -1);
+
 
 
 // Do not edit below this line
