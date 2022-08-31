cc9de1b65a7b1b1d11e1107faa2ded979016f8f1
Nabil <holanabilkhalid@gmail.com>
2022-08-31 19:04:48 +0800

    Added conditional statements & loop to add between two parameters in the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+    let sum = 0;
+    if (y > x){
+    for (let i = x; i <= y; i++ ){
+        sum += i;
+    }
+} else{
+    if (x > y){
+        for (let i = y; i <= x; i++ ){
+            sum += i;
+        }
+    }
+}
+    return sum;  
+}
 
 // Do not edit below this line
 module.exports = sumAll;
