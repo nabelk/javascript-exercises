9775537e7c89757c77d17a2f6bc3b84220ce1ef2
Nabil <holanabilkhalid@gmail.com>
2022-09-02 17:21:33 +0800

    Fix structures of conditional statements in the function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,10 @@
 const leapYears = function(year) {
-    if (year % 4 || year % 400  ){
+    if (year % 400 === 0){
         return true;
-    } else if (year % 100) {
-        return false;
+    } else if (year % 100 === 0){
+            return false;
+    } else if (year % 4 === 0){
+            return true;
     } else {
         return false;
     }
