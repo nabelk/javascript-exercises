6f82cba7b80578192ed7b59c4126e4d0f91b58e9
Nabil <holanabilkhalid@gmail.com>
2022-09-04 19:29:56 +0800

    Added conversion formula from fahrenheit to celsius vice versa in the function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
+  // Convert fahrenheit to celsius
+const ftoc = function(fahrValue) {
+  let result = (fahrValue - 32) * 5/9;
 
+  if (Number.isInteger(result) === true){
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const ctof = function() {
+// Convert celsius to fahrenheit
+const ctof = function(celsValue) {
+  let result = (celsValue * 9/5) + 32;
 
+  if (Number.isInteger(result) === true){
+    return result;
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
