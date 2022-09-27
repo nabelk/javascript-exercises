7e866e47dfe16ccb9c3bedc043bc6c6700c487d9
Nabil <holanabilkhalid@gmail.com>
2022-09-27 23:07:56 +0800

    Added code into the function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {	
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let initial = 0;
+  const sum = arr.forEach(num => {
+    initial += num;
+  });
+
+  return initial;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let initial = 1;
+  const sum = arr.forEach(num => initial *= num);
+  return initial;
 };
 
-const power = function() {
-	
+// console.log(multiply([2,4,6,8,10,12,14]));
+
+const power = function(a,b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+// console.log(power([4,3]));
+
+const factorial = function(num) {
+  let fact = 1;
+  if (num === 0){
+    return 1;
+  } else if (num > 0){
+    for (let i = 1; i <= num; i++ ){
+      fact *= i;
+    }
+  }
+	return fact;
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
