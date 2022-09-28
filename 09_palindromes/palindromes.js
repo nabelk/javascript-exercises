27657ff18be38a09d0e1bb8df5ba7e0689893c72
Nabil <holanabilkhalid@gmail.com>
2022-09-28 19:40:34 +0800

    Added code into the function to check if the given word is palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const punctuation = '!';
+    const lowerCase = word.charAt(0).toLowerCase() + word.slice(1).toLowerCase();
+    let rawWord = lowerCase.split("");
+    const removePunc = rawWord.filter(letter => {
+        return punctuation.indexOf(letter) === - 1;
+    });
+    rawWord = removePunc.join("");
+    const reversecleanword = removePunc.reverse().join("");
 
+    return rawWord === reversecleanword ? true : false;
 };
 
 // Do not edit below this line
