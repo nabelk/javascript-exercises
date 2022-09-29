12fd9dc8775084d5827ff2835484cd9dfbd48178
Nabil <holanabilkhalid@gmail.com>
2022-09-29 21:16:42 +0800

    Added code into the function to return the value of the given fibonacci index number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(indexNum) {
+    let a = 0; // 1st index of fibonacci
+    let b = 1; // 2nd index of fibonacci
+    let c = null;
+    console.log(`Index number = ${c}`);
 
+    for (let i = 2; i <= indexNum; i++ ){
+       console.log(`Round of loop: ${i}`)
+       c = a+ b
+       console.log(`c = (a:${a} + b:${b} = c:${c})`);
+       a = b; // Move var of a to the next index
+       console.log(`a = b which is ${a}`);
+       b = c; // Move var of b to the next index
+       console.log(`b = c  which is ${b}`);
+    }
+
+    console.log (`result: ${c}`);
+    return c;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
