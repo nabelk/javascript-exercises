c0136492f032d1b3b9e68c4e4b327d93715f33f6
Nabil <holanabilkhalid@gmail.com>
2022-09-30 21:34:47 +0800

    Added an array method into the function to return oldest people(array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr){
+    arr.sort((a, b) => {
+        let lastperson, firstperson;
+        if (!a.yearOfDeath || !b.yearOfDeath){
+            lastperson = (new Date().getFullYear()) - a.yearOfBirth;
+            firstperson = (new Date().getFullYear()) - b.yearOfBirth;
+        } else {
+            lastperson = a.yearOfDeath - a.yearOfBirth;
+            firstperson = b.yearOfDeath - b.yearOfBirth;
+        }
+        return lastperson > firstperson ? - 1 : 1;
+     });
 
+    return arr[0];    
 };
 
 // Do not edit below this line
